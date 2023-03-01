b3854da6ee54bb1a919957d4dde2cd356c92f03a
anubhav13singh <singhanubhav81717@gmail.com>
2023-03-01 10:03:28 +0530

    project

---
+++ javascript/ques-1/variable.js
@@ -0,0 +1,23 @@
+ // declareing a primitive data type
+ let name = 'anubhav'; // declared a string
+ console.log(name);
+ let x ;// undefined
+ console.log(x);
+ let y = ''; //null value
+ console.log(y);
+ let open = true;//bolean value
+ console.log(open);
+ let a = 10;//number
+ console.log(a);
+
+ // declaring a non-primitive data type
+ let student = ['rohan','pritam','sonali','deeps','ryan'];
+ console.log(student);
+
+ let office = {
+     empName : 'piyus bansal',
+     empID : 3435,
+     attendence : 90,
+     role : 'floor manager'
+ }
+ console.log(office);
